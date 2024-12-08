f2ba04a08ba37b175b313a229b948ba727f150e5
Jintae Song <w.jinttao@gmail.com>
2024-12-08 22:16:03 +0900

    Feat: add Header, Footer

--- src/widgets/Header.jsx
+++ src/widgets/Header.jsx
@@ -1,5 +1,16 @@
+import Button from "../shared/Button";
+
 function Header() {
-  return <></>;
+  return (
+    <header className="mx-auto">
+      <nav className="flex justify-between h-10 w-full bg-black ">
+        <div className="text-white">logo</div>
+        <div className="mr-10">
+          <Button text="로그인" />
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
