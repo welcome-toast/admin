61afc29b6411e3fcf87ca37d46cd2fd6daa3d612
Jintae Song <w.jinttao@gmail.com>
2024-12-09 23:07:00 +0900

    Refactor: modify user state, session handling

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -13,26 +13,47 @@ import "../index.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [session, setSession] = useState(null);
+  const [user, setUser] = useState({
+    id: "",
+    email: "",
+    displayName: "",
+    photoUrl: "",
+    lastSignInAt: "",
+  });
   const navigate = useNavigate();
 
   useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
+    function setUserInfo(response) {
+      if (response) {
+        const { id, last_sign_in_at: lastSignInAt } = response.user;
+        const { email, full_name: displayName, avatar_url: photoUrl } = response.user.user_metadata;
+        setUser((state) => ({ ...state, id, email, displayName, photoUrl, lastSignInAt }));
+      }
+      return;
+    }
+
+    async function getAuthSession() {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+      setUserInfo(session);
+      if (user.id !== "" && user.id !== undefined && user.id !== null) {
+        navigate("/project");
+      }
+    }
+    getAuthSession();
 
     const {
       data: { subscription },
     } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-
-      if (session !== null) {
+      setUserInfo(session);
+      if (user.id !== "" && user.id !== undefined && user.id !== null) {
         navigate("/project");
       }
     });
 
     return () => subscription.unsubscribe();
-  }, [navigate]);
+  }, [user.id, navigate]);
 
   return (
     <>
@@ -40,7 +61,7 @@ function App() {
       <Routes>
         <Route path="/" exact element={<PageHome />} />
         <Route path="project">
-          <Route index element={<PageProjectList />} />
+          <Route index element={<PageProjectList user={user} />} />
           <Route path=":projectId" exact element={<PageProject />}>
             <Route path="setting" exact element={<ProjectSetting />} />
             <Route path="install" exact element={<ProjectInstall />} />
