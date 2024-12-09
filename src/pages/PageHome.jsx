333314aae21526846743fefe361b402ad0f2e897
Jintae Song <w.jinttao@gmail.com>
2024-12-09 12:14:21 +0900

    Feat: add google OAuth login

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -1,13 +1,33 @@
+import { createClient } from "@supabase/supabase-js";
+
 import Button from "../shared/Button";
 import { CTA_SIGNIN, TITLE_HOME } from "../shared/constant";
 
 function PageHome() {
+  const supabase = createClient(
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_API_KEY,
+  );
+
+  const signInWithGoogle = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: import.meta.env.VITE_SUPABASE_GOOGLE_REDIRECT_URL,
+        queryParams: {
+          access_type: "offline",
+          prompt: "consent",
+        },
+      },
+    });
+  };
+
   return (
     <main className="flex flex-col items-center justify-center gap-10 mt-20 w-full h-screen">
       <div>
         <span className="text-5xl	font-bold">{TITLE_HOME}</span>
       </div>
-      <Button text={CTA_SIGNIN} />
+      <Button text={CTA_SIGNIN} onClick={signInWithGoogle} />
       <div className="flex gap-10">
         <div>image #1</div>
         <div>image #2</div>
--- src/shared/Button.jsx
+++ src/shared/Button.jsx
@@ -1,9 +1,10 @@
 import PropTypes from "prop-types";
 
-function Button({ text }) {
+function Button({ text, onClick }) {
   return (
     <button
       type="button"
+      onClick={onClick}
       className="border-solid border-2 rounded text-white font-medium bg-black border-black hover:bg-zinc-800	hover:border-transparent px-5 py-2"
     >
       {text}
@@ -15,4 +16,5 @@ export default Button;
 
 Button.propTypes = {
   text: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
