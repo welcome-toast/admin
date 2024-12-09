e29d8523c66d37a7232a14e4c0355c00102080dd
Jintae Song <w.jinttao@gmail.com>
2024-12-09 10:47:51 +0900

    Design: style PageHome, Header, Footer

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -1,15 +1,13 @@
 import Button from "../shared/Button";
+import { CTA_SIGNIN, TITLE_HOME } from "../shared/constant";
 
 function PageHome() {
   return (
     <main className="flex flex-col items-center justify-center gap-10 mt-20 w-full h-screen">
       <div>
-        <img alt="logo-white-home" src="/src/assets/logo-white-home.png" width="300" />
+        <span className="text-5xl	font-bold">{TITLE_HOME}</span>
       </div>
-      <div>
-        <span className="text-5xl	font-bold">새로운 유저를 맞이할 준비</span>
-      </div>
-      <Button text="시작하기" />
+      <Button text={CTA_SIGNIN} />
       <div className="flex gap-10">
         <div>image #1</div>
         <div>image #2</div>
--- src/widgets/Header.jsx
+++ src/widgets/Header.jsx
@@ -1,12 +1,15 @@
 import Button from "../shared/Button";
+import { CTA_SIGNIN } from "../shared/constant";
 
 function Header() {
   return (
-    <header className="mx-auto">
-      <nav className="flex justify-between h-10 w-full bg-black ">
-        <div className="text-white">logo</div>
-        <div className="mr-10">
-          <Button text="로그인" />
+    <header className="mx-auto fixed h-max w-full inset-0">
+      <nav className="flex justify-between border-solid border-2 boreder-b-gray-100">
+        <div className="my-5 ml-10">
+          <img alt="logo-white-home" src="/src/assets/logo-header.png" width="100" />
+        </div>
+        <div className="my-5 mr-10">
+          <Button text={CTA_SIGNIN} />
         </div>
       </nav>
     </header>
