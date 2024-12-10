28176f31bc94f44b31e86f530b8ebed5595ffb5a
Jintae Song <w.jinttao@gmail.com>
2024-12-10 20:40:49 +0900

    Refactor: modify actionSaveButton position

--- src/features/ProjectSetting.jsx
+++ src/features/ProjectSetting.jsx
@@ -1,20 +1,14 @@
 import PropTypes from "prop-types";
 
-import Button from "../shared/Button";
 import ActionCardList from "./ActionCardList";
 import ProjectPreview from "./ProjectPreview";
 
 function ProjectSetting({ user, project }) {
-  function handleSaveActionButtonClick() {
-    return;
-  }
-
   return (
     <div className="flex gap-10">
       <section className="flex flex-col gap-5 border-2 border-solid w-[40vw] h-[90vh]">
         <div className="flex justify-between">
           <h3 className="mb-4 text-xl font-bold text-gray-900">액션 에디터</h3>
-          <Button text={"저장"} onClick={handleSaveActionButtonClick} />
         </div>
         <ActionCardList user={user} project={project} />
       </section>
