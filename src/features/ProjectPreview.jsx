ed9e3b9bd3252d347acac68749c71a2dafafefae
Jintae Song <w.jinttao@gmail.com>
2024-12-11 22:58:04 +0900

    Feat: add PagePreview mockup

--- src/features/ProjectPreview.jsx
+++ src/features/ProjectPreview.jsx
@@ -1,5 +1,15 @@
 function ProjectPreview() {
-  return <div className="flex flex-col w-full border-2 border-solid">미리보기 콘텐츠</div>;
+  return (
+    <div className="w-full h-full">
+      <div className="flex flex-col w-full border-2 border-solid">미리보기 콘텐츠</div>
+      <iframe
+        id="projectPreview"
+        title="projectPreview"
+        src="https://www.vanillacoding.co/"
+        className="w-full h-full"
+      />
+    </div>
+  );
 }
 
 export default ProjectPreview;
