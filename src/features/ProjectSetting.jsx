15a805515ee29946cf0852436752e176b62395ac
Jintae Song <w.jinttao@gmail.com>
2024-12-11 22:58:04 +0900

    Feat: add PagePreview mockup

--- src/features/ProjectSetting.jsx
+++ src/features/ProjectSetting.jsx
@@ -6,17 +6,17 @@ import ProjectPreview from "./ProjectPreview";
 function ProjectSetting({ user, project }) {
   return (
     <div className="flex gap-10">
-      <section className="flex flex-col gap-5 border-2 border-solid w-[40vw] h-[90vh]">
+      <section className="flex flex-col gap-5 border-2 border-solid w-[20vw] h-[90vh]">
         <div className="flex justify-between">
           <h3 className="mb-4 text-xl font-bold text-gray-900">액션 에디터</h3>
         </div>
         <ActionCardList user={user} project={project} />
       </section>
-      <section className="flex flex-col gap-5 border-2 border-solid w-[40vw] h-[90vh]">
+      <section className="flex flex-col gap-5 border-2 border-solid w-[70vw] h-[90vh]">
         <div className="flex justify-between">
           <h3 className="mb-4 text-xl font-bold text-gray-900">미리 보기</h3>
         </div>
-        <div>
+        <div className="w-full h-full">
           <ProjectPreview />
         </div>
       </section>
