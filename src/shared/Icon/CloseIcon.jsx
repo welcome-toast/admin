b42188e94442e1f196480d15e0f832f191b34ede
Jintae Song <w.jinttao@gmail.com>
2024-12-13 22:57:57 +0900

    Feat: add close icon

---
+++ src/shared/Icon/CloseIcon.jsx
@@ -0,0 +1,18 @@
+const CloseIcon = () => {
+  return (
+    <svg
+      id="closeIcon"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className="size-6"
+    >
+      <title id="closeIcon">closeIcon</title>
+      <path id="closeIcon" d="M6 18 18 6M6 6l12 12" />
+    </svg>
+  );
+};
+
+export default CloseIcon;
