b4f8d3d67befb002732479c1e6cf1046307790f6
Jintae Song <w.jinttao@gmail.com>
2024-12-13 22:57:43 +0900

    Feat: add modal container mockup

--- src/pages/PageProjectList.jsx
+++ src/pages/PageProjectList.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../shared/supabase";
+import ModalContainer from "../widgets/ModalContainer";
 
 const initialProject = [
   {
@@ -53,24 +54,29 @@ function PageProjectList({ user }) {
   }, [user.id, user]);
 
   return (
-    <section className="flex flex-col items-center justify-center gap-10 mt-20 w-full h-screen">
-      <h3>프로젝트 리스트</h3>
-      <div className="flex gap-5">
-        {project?.map((proejct) => (
-          <button
-            key={proejct.id}
-            type="button"
-            onClick={() => handleProjectClick(proejct.id)}
-            className="border-solid border-2 rounded bg-black text-white p-10"
-          >
-            <ul className="flex flex-col gap-3">
-              <li>{proejct.name}</li>
-              <li>{proejct.created_at}</li>
-              <li>{proejct.link}</li>
-              <li>{proejct.is_installed ? "true" : "false"}</li>
-            </ul>
-          </button>
-        ))}
+    <section className="flex items-center justify-center gap-10 mt-20 w-full h-screen">
+      <div className="flex flex-col">
+        <h3 className="mb-4 text-xl font-bold text-gray-900">프로젝트 리스트</h3>
+        <div className="flex flex-col gap-5 border-2 border-solid rounded p-5">
+          {project?.map((proejct) => (
+            <button
+              key={proejct.id}
+              type="button"
+              onClick={() => handleProjectClick(proejct.id)}
+              className="border-solid border-2 rounded bg-black text-white p-10"
+            >
+              <ul className="flex gap-3">
+                <li>{proejct.name}</li>
+                <li>{proejct.created_at}</li>
+                <li>{proejct.link}</li>
+                <li>{proejct.is_installed ? "true" : "false"}</li>
+              </ul>
+            </button>
+          ))}
+        </div>
+      </div>
+      <div className="">
+        <ModalContainer />
       </div>
     </section>
   );
