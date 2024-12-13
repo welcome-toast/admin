51185652c1b9bcd9fac178e0d19b009e845c5e4b
Jintae Song <w.jinttao@gmail.com>
2024-12-13 20:22:36 +0900

    Refactor: improve signIn common logic

--- src/shared/supabase.js
+++ src/shared/supabase.js
@@ -5,4 +5,17 @@ const supabase = createClient(
   import.meta.env.VITE_SUPABASE_API_KEY,
 );
 
-export { supabase };
+async function signIn() {
+  await supabase.auth.signInWithOAuth({
+    provider: "google",
+    options: {
+      redirectTo: import.meta.env.VITE_SUPABASE_GOOGLE_REDIRECT_URL,
+      queryParams: {
+        access_type: "offline",
+        prompt: "consent",
+      },
+    },
+  });
+}
+
+export { supabase, signIn };
