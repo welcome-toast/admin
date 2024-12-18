d4f2f029f53375b8814f4a416bd711db2bcac2c2
Jintae Song <w.jinttao@gmail.com>
2024-12-18 12:02:30 +0900

    Design: style project title section

--- src/pages/PageProject.jsx
+++ src/pages/PageProject.jsx
@@ -7,10 +7,8 @@ function PageProject() {
   const project = location.state?.project;
 
   return (
-    <section className="mt-40 flex h-screen w-full flex-col items-center justify-center gap-10">
-      <h3 className="mb-4 font-extrabold text-gray-900 text-xl">
-        {project?.name ? project.name : ""}
-      </h3>
+    <section className="mt-32 flex h-screen w-full flex-col items-center justify-center gap-5">
+      <h3 className="font-extrabold text-2xl text-gray-900">{project?.name ? project.name : ""}</h3>
       <div className="flex gap-10">
         <button type="button">설정</button>
         <button type="button">연동</button>
