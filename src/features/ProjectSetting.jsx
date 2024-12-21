af5a91218727ea9d90a4cf2bed020d66dbfe5ef2
Jintae Song <w.jinttao@gmail.com>
2024-12-21 19:59:00 +0900

    Refactor: remove flag isToastSavedRef

--- src/features/ProjectSetting.jsx
+++ src/features/ProjectSetting.jsx
@@ -1,12 +1,11 @@
 import PropTypes from "prop-types";
 
-import { useRef, useState } from "react";
+import { useState } from "react";
 import ProjectPreview from "./ProjectPreview";
 import ToastCardList from "./ToastCardList";
 
 function ProjectSetting({ project }) {
   const [previewRef, setPreviewRef] = useState(null);
-  const isToastSavedRef = useRef(false);
 
   return (
     <div className="flex gap-10">
@@ -14,11 +13,7 @@ function ProjectSetting({ project }) {
         <div className="flex flex-col">
           <h3 className="mb-4 font-bold text-gray-900 text-xl">토스트 에디터</h3>
         </div>
-        <ToastCardList
-          project={project}
-          previewRef={previewRef}
-          isToastSavedRef={isToastSavedRef}
-        />
+        <ToastCardList project={project} previewRef={previewRef} />
       </section>
       <section className="flex h-[90vh] w-[70vw] flex-col gap-5 border-2 border-solid">
         <div className="flex justify-between">
