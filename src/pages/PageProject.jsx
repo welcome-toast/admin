2bf9ee0f7a445e4e6d4a9b97d3dde7eec94f2648
Jintae Song <w.jinttao@gmail.com>
2024-12-22 20:59:17 +0900

    Feat: add toast list mockup

--- src/pages/PageProject.jsx
+++ src/pages/PageProject.jsx
@@ -66,11 +66,20 @@ function PageProject() {
   return (
     <div className="mt-32 flex h-screen w-screen gap-5 px-2">
       <section className="flex h-[90vh] w-[10vw] flex-col gap-5 border-2 border-solid">
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-5">
           <h3 className="mb-4 font-bold text-gray-900 text-xl">토스트 리스트</h3>
           {toastList.map((toastSaved) => (
-            <div key={toastSaved.id}>{toastSaved.name}</div>
+            <button type="button" key={toastSaved.id} className="flex flex-col gap-1 border-2">
+              <span className="mb-1 font-bold text-gray-900 text-xl">{toastSaved.name}</span>
+              <span className="mb-1 text-gray-900 text-l">{toastSaved.message_title}</span>
+              <span className="mb-1 text-gray-500">업데이트 {toastSaved.updated_at}</span>
+            </button>
           ))}
+          <div className="mb-5">
+            <button type="button" className="h-14 w-full border-2 border-black text-2xl">
+              +
+            </button>
+          </div>
         </div>
       </section>
       <section className="flex h-[90vh] w-[70vw] flex-col gap-5 border-2 border-solid">
