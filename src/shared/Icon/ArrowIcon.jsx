cf4b6b693e50c1c80d52839568b83d77a3220bd8
Jintae Song <w.jinttao@gmail.com>
2024-12-22 14:13:46 +0900

    Feat: add arrow icon

---
+++ src/shared/Icon/ArrowIcon.jsx
@@ -0,0 +1,23 @@
+const ArrowIcon = () => {
+  return (
+    <svg
+      id="arrowIcon"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className="size-6"
+    >
+      <title id="arrowIcon">arrowIcon</title>
+      <path
+        id="arrowIcon"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m19.5 8.25-7.5 7.5-7.5-7.5"
+      />
+    </svg>
+  );
+};
+
+export default ArrowIcon;
