85d6794be70d16a45f0fcdb10a3fa92b25b50192
Jintae Song <w.jinttao@gmail.com>
2024-12-22 19:08:26 +0900

    Refactor: remove unused tab division

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 
-import ProjectSetting from "../features/ProjectSetting";
-import ProjectInstall from "../features/ProjectSetting";
 import PageError from "../pages/PageError";
 import PageHome from "../pages/PageHome";
 import PageProject from "../pages/PageProject";
@@ -41,10 +39,7 @@ function App() {
         <Route path="/" exact element={<PageHome user={user} setUser={setUser} />} />
         <Route path="project">
           <Route index element={<PageProjectList user={user} />} />
-          <Route path=":projectId" exact element={<PageProject />}>
-            <Route path="setting" exact element={<ProjectSetting />} />
-            <Route path="install" exact element={<ProjectInstall />} />
-          </Route>
+          <Route path="setting/:projectId" exact element={<PageProject />} />
         </Route>
         <Route path="*" element={<PageError />} />
       </Routes>
--- src/pages/PageProject.jsx
+++ src/pages/PageProject.jsx
@@ -1,25 +1,36 @@
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
-import ProjectInstall from "../features/ProjectInstall";
-import ProjectSetting from "../features/ProjectSetting";
+
+import ProjectPreview from "../features/ProjectPreview";
+import ToastCardList from "../features/ToastCardList";
 
 function PageProject() {
   const location = useLocation();
   const project = location.state?.project;
+  const [previewRef, setPreviewRef] = useState(null);
 
   return (
-    <section className="mt-32 flex h-screen w-full flex-col items-center justify-center gap-5">
-      <h3 className="font-extrabold text-2xl text-gray-900">{project?.name ? project.name : ""}</h3>
-      <div className="flex gap-10">
-        <button type="button">설정</button>
-        <button type="button">연동</button>
-      </div>
-      <div>
-        <ProjectSetting project={project} />
-      </div>
-      <div>
-        <ProjectInstall />
-      </div>
-    </section>
+    <div className="flex h-screen w-screen gap-5 px-2">
+      <section className="flex h-[90vh] w-[10vw] flex-col gap-5 border-2 border-solid">
+        <div className="flex">
+          <h3 className="mb-4 font-bold text-gray-900 text-xl">토스트 리스트</h3>
+        </div>
+      </section>
+      <section className="flex h-[90vh] w-[70vw] flex-col gap-5 border-2 border-solid">
+        <div className="flex justify-between">
+          <h3 className="mb-4 font-bold text-gray-900 text-xl">미리 보기</h3>
+        </div>
+        <div className="h-full w-full">
+          <ProjectPreview project={project} setPreviewRef={setPreviewRef} />
+        </div>
+      </section>
+      <section className="flex h-[90vh] w-[20vw] flex-col gap-5 border-2 border-solid">
+        <div className="flex flex-col">
+          <h3 className="mb-4 font-bold text-gray-900 text-xl">토스트 에디터</h3>
+        </div>
+        <ToastCardList project={project} previewRef={previewRef} />
+      </section>
+    </div>
   );
 }
 
