c542096647909ff7537ae6b0575db5372faabb41
Jintae Song <w.jinttao@gmail.com>
2025-01-07 15:34:31 +0900

    Refactor: apply ToastCard component, modify style

--- src/pages/PageProject.jsx
+++ src/pages/PageProject.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 import ProjectPreview from "../features/ProjectPreview";
+import ToastCard from "../features/ToastCard";
 import ToastCardEditor from "../features/ToastCardEditor";
 import { supabase } from "../shared/supabase";
 
@@ -90,50 +91,37 @@ function PageProject() {
   }, [project.link, toastList]);
 
   return (
-    <div className="mt-32 flex h-screen w-screen gap-5 px-2">
-      <section className="flex h-[90vh] w-[10vw] flex-col gap-5 border-2 border-solid">
-        <div className="flex flex-col gap-5">
-          <h3 className="mb-4 font-bold text-gray-900 text-xl">토스트 리스트</h3>
+    <div className="flex h-fit w-screen overflow-scroll px-3 [&::-webkit-scrollbar]:hidden">
+      <section className="flex h-[90vh] w-[12vw] flex-col gap-5">
+        <div className="flex w-full flex-col gap-5">
+          <h3 className="mt-3 font-bold text-gray-900 text-lg">토스트 리스트</h3>
           {toastList.map((toastSaved, index) => (
-            <button
-              type="button"
+            <ToastCard
               key={toastSaved.id}
-              onClick={() => handleToastCardClick(index)}
-              className="flex flex-col gap-1 border-2"
-            >
-              <span className="mb-1 font-bold text-gray-900 text-xl">{toastSaved.name}</span>
-              <span className="mb-1 text-gray-900 text-l">{toastSaved.message_title}</span>
-              <span className="mb-1 text-gray-500">업데이트 {toastSaved.updated_at}</span>
-            </button>
+              toastSaved={toastSaved}
+              index={index}
+              handleToastCardClick={() => handleToastCardClick(index)}
+            />
           ))}
           <div className="mb-5">
             <button
               type="button"
               onClick={handleNewToastButtonClick}
-              className="h-14 w-full border-2 border-black text-2xl"
+              className="h-14 w-full rounded border-2 border-gray-500 text-2xl hover:bg-gray-300"
             >
               +
             </button>
           </div>
         </div>
       </section>
-      <section className="flex h-[90vh] w-[70vw] flex-col gap-5 border-2 border-solid">
-        <div className="flex justify-between">
-          <h3 className="mb-4 font-bold text-gray-900 text-xl">미리 보기</h3>
-        </div>
+      <section className="flex h-[90vh] w-[70vw] flex-col gap-5 px-1">
         <div className="h-full w-full">
           <ProjectPreview project={project} setPreviewRef={setPreviewRef} />
         </div>
       </section>
-      <section className="flex h-[90vh] w-[20vw] flex-col gap-5 border-2 border-solid">
-        <div className="flex flex-col">
-          <h3 className="font-bold text-gray-900 text-xl">토스트 에디터</h3>
-        </div>
+      <section className="flex h-[90vh] w-[20vw] flex-col gap-5">
         {indexToastForEdit >= 0 && toastList.length > 0 ? (
           <>
-            <div className="w-full bg-slate-200 p-3">
-              <span>{toastList[indexToastForEdit].name}</span>
-            </div>
             <ToastCardEditor
               toast={toastList[indexToastForEdit]}
               setToastList={setToastList}
