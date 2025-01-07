32a3693e37069485979ead2c8f7c2827d2818871
Jintae Song <w.jinttao@gmail.com>
2025-01-07 15:38:09 +0900

    Design: modify ProjectCard color, font style

--- src/features/ProjectCard.jsx
+++ src/features/ProjectCard.jsx
@@ -5,11 +5,11 @@ function ProjectCard({ project, handleProjectClick }) {
   const date = getDate(project.created_at);
 
   return (
-    <div className="flex w-full max-w-sm flex-col rounded border bg-gray-800 p-6 text-white shadow-xl hover:bg-gray-600">
+    <div className="flex w-full max-w-sm flex-col rounded border bg-gray-900 p-6 text-white shadow-xl hover:bg-gray-700">
       <button key={project.id} type="button" onClick={() => handleProjectClick(project.id)}>
         <ul className="p-2">
-          <li>{project.name}</li>
-          <li className="my-1 text-gray-300">{project.link}</li>
+          <li className="mb-3 text-lg">{project.name}</li>
+          <li className="my-1 font-extralight text-gray-300 text-sm italic">{project.link}</li>
           <li className="mt-5 text-gray-400 text-sm">
             생성 일자{" "}
             {`${date.year}. ${date.month}. ${date.currentDate}. ${date.currentHour}:${date.currentMinute}`}
