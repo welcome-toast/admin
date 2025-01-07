49f73d783345e9ce8cf4f4621a35be3c83ec82a2
Jintae Song <w.jinttao@gmail.com>
2025-01-07 15:33:48 +0900

    Refactor: modify footer in PageProject

--- src/widgets/Footer.jsx
+++ src/widgets/Footer.jsx
@@ -1,4 +1,11 @@
+import { useLocation } from "react-router-dom";
+
 function Footer() {
+  const location = useLocation();
+  if (location.pathname.includes("toast")) {
+    return null;
+  }
+
   return (
     <footer className="w-full py-5 text-center">
       <div>Footer</div>
