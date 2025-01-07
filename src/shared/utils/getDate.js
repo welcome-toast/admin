923e1d149ece3c433b9b524835cf6a42f94c02b7
Jintae Song <w.jinttao@gmail.com>
2025-01-07 13:47:14 +0900

    Feat: add getDate util function

---
+++ src/shared/utils/getDate.js
@@ -0,0 +1,21 @@
+const getDate = (dateString) => {
+  if (dateString === null || dateString === undefined) {
+    return null;
+  }
+
+  const date = new Date(dateString);
+
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+
+  const currentDate = date.getDate();
+  const currentDay = date.getDay();
+
+  const currentHour =
+    date.getHours() > 12 ? `오후 ${date.getHours() - 12}` : `오전 ${date.getHours()}`;
+  const currentMinute = date.getMinutes();
+
+  return { year, month, currentDate, currentDay, currentHour, currentMinute };
+};
+
+export { getDate };
