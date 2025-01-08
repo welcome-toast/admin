698ac59a966605bfcb3bbd4e80d61532d681a2e3
Jintae Song <w.jinttao@gmail.com>
2025-01-09 04:08:21 +0900

    Refactor: modify PageHome text constants

--- src/shared/constant.js
+++ src/shared/constant.js
@@ -1,3 +1,3 @@
-export const CTA_SIGNIN = "<script> 한 줄로 끝내기";
+export const TITLE_HOME = "새로운 유저의 눈길을 끄는 방법";
+export const CTA_SIGNIN = "🧑‍💻 코드 한 줄로 끝내기";
 export const CTA_SIGNOUT = "로그아웃";
-export const TITLE_HOME = "새로운 유저의 눈길을 끌고 싶다면,";
