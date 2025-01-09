65965de020854242df107b3965280bd2a6d3224c
Jintae Song <w.jinttao@gmail.com>
2025-01-09 22:26:56 +0900

    Feat: add preview loading ui

--- src/features/ProjectPreview.jsx
+++ src/features/ProjectPreview.jsx
@@ -1,16 +1,37 @@
 import PropTypes from "prop-types";
-import { forwardRef } from "react";
+import { forwardRef, useEffect, useState } from "react";
+import Loading from "../shared/Loading";
 
 const ProjectPreview = forwardRef(function ProjectPreview({ project }, ref) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    function handlePreviewLoad(e) {
+      if (project.link.includes(e.origin) && e.data.isPreviewLoaded) {
+        setIsLoading(false);
+      }
+      return;
+    }
+
+    window.addEventListener("message", handlePreviewLoad);
+
+    return () => window.removeEventListener("message", handlePreviewLoad);
+  }, [project?.link]);
+
   return (
-    <div className="h-full w-full px-1">
+    <div className="flex h-full w-full justify-center px-1">
+      {isLoading && (
+        <div className="mt-10 p-10">
+          <Loading />
+        </div>
+      )}
       <iframe
         id="projectPreview"
         title="projectPreview"
         ref={ref}
         src={project?.link}
         loading="lazy"
-        className="h-full w-full rounded-sm p-1"
+        className={`${isLoading ? "h-1 w-1" : "h-full w-full"} rounded-sm p-1`}
       />
     </div>
   );
