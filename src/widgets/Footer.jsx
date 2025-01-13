9efefd7de8304d8717cf6f8a169f8e3e323bc1bb
Jintae Song <w.jinttao@gmail.com>
2025-01-13 19:59:24 +0900

    Feat: add footer github icon, repository link

--- src/widgets/Footer.jsx
+++ src/widgets/Footer.jsx
@@ -7,8 +7,22 @@ function Footer() {
   }
 
   return (
-    <footer className="w-full py-5 text-center">
-      <div>Footer</div>
+    <footer className="my-1 w-full text-center">
+      <div className="mx-8 mb-4 flex justify-end">
+        <a
+          href="https://github.com/welcome-toast/welcome-toast?tab=readme-ov-file#welcome-toast"
+          target="_blank"
+          rel="noreferrer"
+          className="flex items-center text-left hover:text-gray-500"
+        >
+          <img alt="logo-github" src="/assets/logo-footer-github.png" width="40rem" />
+          <span className="mx-3 font-semibold">
+            웰컴토스트를
+            <br />
+            소개해요
+          </span>
+        </a>
+      </div>
     </footer>
   );
 }
