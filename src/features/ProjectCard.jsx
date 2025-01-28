dfcc0cc82f5807764bf1bb642b8c82b5f7288c78
Jintae Song <w.jinttao@gmail.com>
2025-01-28 13:15:14 +0900

    Design: modify ProjectCard layout

--- src/features/ProjectCard.jsx
+++ src/features/ProjectCard.jsx
@@ -5,18 +5,21 @@ function ProjectCard({ project, handleProjectClick }) {
   const date = getDate(project.created_at);
 
   return (
-    <div className="flex w-full max-w-sm flex-col rounded border bg-gray-900 p-6 text-white shadow-xl hover:bg-gray-700">
-      <button key={project.id} type="button" onClick={() => handleProjectClick(project.id)}>
-        <ul className="p-2">
-          <li className="mb-3 font-semibold text-2xl">{project.name}</li>
-          <li className="my-1 font-extralight text-gray-300 text-sm italic">{project.link}</li>
-          <li className="mt-5 text-gray-400 text-sm">
-            생성 일자{" "}
-            {`${date.year}. ${date.month}. ${date.currentDate}. ${date.currentHour}:${date.currentMinute}`}
-          </li>
-        </ul>
-      </button>
-    </div>
+    <button
+      key={project.id}
+      type="button"
+      className="flex w-full max-w-sm flex-col items-center justify-center rounded border bg-gray-900 p-6 text-white shadow-xl hover:bg-gray-700"
+      onClick={() => handleProjectClick(project.id)}
+    >
+      <ul className="p-2">
+        <li className="mb-3 font-semibold text-2xl">{project.name}</li>
+        <li className="my-1 font-extralight text-gray-300 text-sm italic">{project.link}</li>
+        <li className="mt-5 text-gray-400 text-sm">
+          생성 일자{" "}
+          {`${date.year}. ${date.month}. ${date.currentDate}. ${date.currentHour}:${date.currentMinute}`}
+        </li>
+      </ul>
+    </button>
   );
 }
 
