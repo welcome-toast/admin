64b1b5a4bfb34aab97a83e8af1e6d58313daa168
Jintae Song <w.jinttao@gmail.com>
2025-01-30 17:04:54 +0900

    Feat: add hasCloseButton flag in ModalContainer

--- src/widgets/ModalContainer.jsx
+++ src/widgets/ModalContainer.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import CloseIcon from "../shared/Icon/CloseIcon";
 
-function ModalContainer({ children, modalTitle, setIsOpenModal }) {
+function ModalContainer({ children, modalTitle, hasCloseButton = true, setIsOpenModal }) {
   function handleCloseButtonClick() {
     setIsOpenModal(false);
   }
@@ -10,9 +10,11 @@ function ModalContainer({ children, modalTitle, setIsOpenModal }) {
     <div className="w-full max-w-lg rounded bg-white p-10 md:w-lg">
       <div className="flex justify-between">
         <h3 className="font-bold text-gray-900 text-xl">{modalTitle}</h3>
-        <button type="button" onClick={handleCloseButtonClick}>
-          <CloseIcon />
-        </button>
+        {hasCloseButton && (
+          <button type="button" onClick={handleCloseButtonClick}>
+            <CloseIcon />
+          </button>
+        )}
       </div>
       {children}
     </div>
@@ -24,5 +26,6 @@ export default ModalContainer;
 ModalContainer.propTypes = {
   children: PropTypes.object.isRequired,
   modalTitle: PropTypes.string.isRequired,
-  setIsOpenModal: PropTypes.func.isRequired,
+  hasCloseButton: PropTypes.bool,
+  setIsOpenModal: PropTypes.func,
 };
