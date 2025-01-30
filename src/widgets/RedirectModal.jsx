3843d7f400f52fcdeb5c96730c08da39e8a1eee9
Jintae Song <w.jinttao@gmail.com>
2025-01-30 15:23:23 +0900

    Feat: add RedirectModal mockup

---
+++ src/widgets/RedirectModal.jsx
@@ -0,0 +1,12 @@
+import ModalBackground from "./ModalBackground";
+import ModalContainer from "./ModalContainer";
+
+function RedirectModal() {
+  return (
+    <ModalBackground>
+      <ModalContainer>{"content"}</ModalContainer>
+    </ModalBackground>
+  );
+}
+
+export default RedirectModal;
