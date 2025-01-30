57c9c6a4de1695160a5e50c8730919c965965059
Jintae Song <w.jinttao@gmail.com>
2025-01-30 14:42:21 +0900

    Feat: add handler close modal when click modal background

--- src/widgets/ModalBackground.jsx
+++ src/widgets/ModalBackground.jsx
@@ -1,11 +1,23 @@
 import PropTypes from "prop-types";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
-function ModalBackground({ children }) {
-  const outsideRef = useRef(null);
+function ModalBackground({ children, setIsOpenModal }) {
+  const backgroundRef = useRef(null);
+
+  useEffect(() => {
+    function handleModalBackgroundClick(event) {
+      if (event?.target === backgroundRef.current) {
+        setIsOpenModal(false);
+      }
+    }
+
+    window.addEventListener("click", handleModalBackgroundClick);
+    return () => window.removeEventListener("click", handleModalBackgroundClick);
+  }, [setIsOpenModal]);
+
   return (
     <div
-      ref={outsideRef}
+      ref={backgroundRef}
       className="fixed top-0 left-0 flex h-screen w-full flex-col items-center justify-center gap-5 border-2 border-black border-solid bg-black bg-opacity-70 p-18"
     >
       {children}
@@ -17,4 +29,5 @@ export default ModalBackground;
 
 ModalBackground.propTypes = {
   children: PropTypes.object.isRequired,
+  setIsOpenModal: PropTypes.func.isRequired,
 };
