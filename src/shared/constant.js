00611fddc729dc256623d2a734d182cd0409b3fa
Jintae Song <w.jinttao@gmail.com>
2025-01-30 17:06:55 +0900

    Feat: add modal description constant

--- src/shared/constant.js
+++ src/shared/constant.js
@@ -1,6 +1,8 @@
 export const TITLE_HOME = "새로운 유저의 눈길을 끄는 방법";
 export const CTA_SIGNIN = "🧑‍💻 코드 한 줄로 끝내기";
 export const CTA_SIGNOUT = "로그아웃";
+export const DESC_REDIRECT_API_KEY_ACCESS =
+  "프로젝트의 API Key로 연동된 웹사이트만 확인할 수 있습니다. 연동 여부 혹은 프로젝트 목록에서 API Key를 확인해주세요.";
 
 export const INITIAL_PROJECT = [
   {
