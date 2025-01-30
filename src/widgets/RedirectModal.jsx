a1db6f416e567033a5f7a7da0648c77564937b54
Jintae Song <w.jinttao@gmail.com>
2025-01-30 17:08:23 +0900

    Feat: add RedirectModal redirect logic

--- src/widgets/RedirectModal.jsx
+++ src/widgets/RedirectModal.jsx
@@ -1,12 +1,31 @@
+import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
+import Button from "../shared/Button";
 import ModalBackground from "./ModalBackground";
 import ModalContainer from "./ModalContainer";
 
-function RedirectModal() {
+function RedirectModal({ text, route }) {
+  const navigate = useNavigate();
+
+  function handleRedirectButtonClick(redirectRoute) {
+    navigate(`${redirectRoute}`);
+  }
+
   return (
-    <ModalBackground>
-      <ModalContainer>{"content"}</ModalContainer>
+    <ModalBackground canCloseBackgroundClick={false}>
+      <ModalContainer modalTitle={"안내"} hasCloseButton={false}>
+        <div className="my-5 flex justify-center text-center">{text}</div>
+        <div className="flex justify-center">
+          <Button text={"확인"} onClick={() => handleRedirectButtonClick(route)} />
+        </div>
+      </ModalContainer>
     </ModalBackground>
   );
 }
 
 export default RedirectModal;
+
+RedirectModal.propTypes = {
+  text: PropTypes.string.isRequired,
+  route: PropTypes.string.isRequired,
+};
