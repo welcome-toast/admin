6b86e958d128cdca6b8a583f33b48d8e5681c836
Jintae Song <w.jinttao@gmail.com>
2025-01-30 14:35:29 +0900

    Feat: add modal close button click handler

--- src/widgets/ModalContainer.jsx
+++ src/widgets/ModalContainer.jsx
@@ -1,12 +1,16 @@
 import PropTypes from "prop-types";
 import CloseIcon from "../shared/Icon/CloseIcon";
 
-function ModalContainer({ ModalTitle }) {
+function ModalContainer({ ModalTitle, setIsOpenModal }) {
+  function handleCloseButtonClick() {
+    setIsOpenModal(false);
+  }
+
   return (
     <div className="w-full rounded bg-white p-10 md:w-1/3">
       <div className="flex justify-between">
         <h3 className="font-bold text-gray-900 text-xl">{ModalTitle}</h3>
-        <button type="button">
+        <button type="button" onClick={handleCloseButtonClick}>
           <CloseIcon />
         </button>
       </div>
@@ -18,4 +22,5 @@ export default ModalContainer;
 
 ModalContainer.propTypes = {
   ModalTitle: PropTypes.string.isRequired,
+  setIsOpenModal: PropTypes.func.isRequired,
 };
