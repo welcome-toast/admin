f0c865b96ef2891178c9757c42228840e3e361e2
Jintae Song <w.jinttao@gmail.com>
2025-01-30 18:29:52 +0900

    Feat: add ProjectDetailDropdown handler basic logic

--- src/features/ProjectCard.jsx
+++ src/features/ProjectCard.jsx
@@ -1,14 +1,23 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
+import KebabIcon from "../shared/Icon/KebabIcon";
 import { getDate } from "../shared/utils/getDate";
+import ProjectDetailsDropdown from "../widgets/ProjectDetailsDropdown";
 
 function ProjectCard({ project, handleProjectClick }) {
+  const [isOpenDropdown, setIsOpenDropdown] = useState(false);
   const date = getDate(project.created_at);
+
+  function handleProjectDetailClick(event) {
+    event.stopPropagation();
+    setIsOpenDropdown(true);
+  }
 
   return (
     <button
       key={project.id}
       type="button"
-      className="flex w-full max-w-sm flex-col items-center justify-center rounded border border-gray-300 bg-gray-100 p-6 text-black shadow-xl hover:border-2 hover:border-blue-700 hover:bg-blue-100"
+      className="relative flex w-full max-w-sm flex-col items-center justify-center rounded border border-gray-300 bg-gray-100 p-6 text-black shadow-xl hover:border-2 hover:border-blue-700 hover:bg-blue-100"
       onClick={() => handleProjectClick(project.id)}
     >
       <ul className="p-2">
@@ -19,6 +28,14 @@ function ProjectCard({ project, handleProjectClick }) {
           {`${date.year}. ${date.month}. ${date.currentDate}. ${date.currentHour}:${date.currentMinute}`}
         </li>
       </ul>
+      <button
+        type="button"
+        onClick={handleProjectDetailClick}
+        className="absolute top-2 right-1 p-1"
+      >
+        <KebabIcon />
+      </button>
+      {isOpenDropdown && <ProjectDetailsDropdown setIsOpenDropdown={setIsOpenDropdown} />}
     </button>
   );
 }
--- src/widgets/ProjectDetailsDropdown.jsx
+++ src/widgets/ProjectDetailsDropdown.jsx
@@ -1,6 +1,25 @@
-function ProjectDetailsDropdown() {
+import PropTypes from "prop-types";
+import { useEffect, useRef } from "react";
+
+function ProjectDetailsDropdown({ setIsOpenDropdown }) {
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    function handleDropdownOutsideClick(event) {
+      if (event.target !== dropdownRef.current) {
+        setIsOpenDropdown(false);
+      }
+    }
+
+    window.addEventListener("click", handleDropdownOutsideClick);
+    return () => window.removeEventListener("click", handleDropdownOutsideClick);
+  }, [setIsOpenDropdown]);
+
   return (
-    <ul className="absolute top-10 right-2 flex flex-col gap-2 rounded border-2 border-gray-300 bg-gray-100 p-5">
+    <ul
+      ref={dropdownRef}
+      className="absolute top-10 right-2 flex flex-col gap-2 rounded border-2 border-gray-300 bg-gray-100 p-5"
+    >
       <li>연동 스크립트</li>
       <li>프로젝트 삭제</li>
     </ul>
@@ -8,3 +27,7 @@ function ProjectDetailsDropdown() {
 }
 
 export default ProjectDetailsDropdown;
+
+ProjectDetailsDropdown.propTypes = {
+  setIsOpenDropdown: PropTypes.func.isRequired,
+};
