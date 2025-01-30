c2fce298878ced94638f54da37233868ba5b95b2
Jintae Song <w.jinttao@gmail.com>
2025-01-30 14:35:06 +0900

    Feat: add ModalBackground ref

--- src/widgets/ModalBackground.jsx
+++ src/widgets/ModalBackground.jsx
@@ -1,8 +1,13 @@
 import PropTypes from "prop-types";
+import { useRef } from "react";
 
 function ModalBackground({ children }) {
+  const outsideRef = useRef(null);
   return (
-    <div className="fixed top-0 left-0 flex h-screen w-full flex-col items-center justify-center gap-5 border-2 border-black border-solid bg-black bg-opacity-70 p-18">
+    <div
+      ref={outsideRef}
+      className="fixed top-0 left-0 flex h-screen w-full flex-col items-center justify-center gap-5 border-2 border-black border-solid bg-black bg-opacity-70 p-18"
+    >
       {children}
     </div>
   );
