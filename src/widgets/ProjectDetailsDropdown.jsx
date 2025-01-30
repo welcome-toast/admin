e94fc4b14b26d1c37dbc02ed9d52059953d250ad
Jintae Song <w.jinttao@gmail.com>
2025-01-30 18:07:42 +0900

    Feat: add ProjectDetailsDropDown mockup

---
+++ src/widgets/ProjectDetailsDropdown.jsx
@@ -0,0 +1,10 @@
+function ProjectDetailsDropdown() {
+  return (
+    <ul className="absolute top-10 right-2 flex flex-col gap-2 rounded border-2 border-gray-300 bg-gray-100 p-5">
+      <li>연동 스크립트</li>
+      <li>프로젝트 삭제</li>
+    </ul>
+  );
+}
+
+export default ProjectDetailsDropdown;
