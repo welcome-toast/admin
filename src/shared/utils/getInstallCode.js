67f8f08d0e7b35502e831cf0c23a6fa4069e3546
Jintae Song <w.jinttao@gmail.com>
2025-01-31 20:57:14 +0900

    Feat: add getInstallCode snippet util function

---
+++ src/shared/utils/getInstallCode.js
@@ -0,0 +1,16 @@
+function getInstallCode(apiKey) {
+  const VERSION = "1.0.0";
+  const SDK_URL = `https://cdn.jsdelivr.net/gh/welcome-toast/welcome-toast@refs/heads/v${VERSION}/src/main.js`;
+
+  return `<script>
+  window.welcometoastConfig = {
+    apiKey: "${apiKey}",
+    init: function() {
+      window.welcometoast.getProject(window.welcometoastConfig.apiKey);
+    }
+  };
+</script>
+<script type="text/javascript" src="${SDK_URL}"></script>`;
+}
+
+export { getInstallCode };
