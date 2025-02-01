41edebd4e986921ead566f0551886fd266c53fcd
Jintae Song <w.jinttao@gmail.com>
2025-02-01 17:03:41 +0900

    Feat: apply install guide constant, modify styles

--- src/widgets/InstallGuide.jsx
+++ src/widgets/InstallGuide.jsx
@@ -1,18 +1,28 @@
-import { INSTALL_GUIDE_STEPS } from "../shared/constant";
+import CheckIcon from "../shared/Icon/CheckIcon";
+import { DESC_INSTALL_GUIDE, INSTALL_GUIDE_STEPS, TITLE_INSTALL_GUIDE } from "../shared/constant";
 
 function InstallGuide() {
   return (
-    <section className="h-fit bg-gray-100">
-      <div className="my-3">
-        <h3 className="mb-4 font-bold text-gray-900 text-xl">웰컴토스트 이용 가이드</h3>
-        <span className="ml-3 font-normal">준비물 : 배포된 웹사이트</span>
+    <section className="mb-10 h-fit">
+      <div className="my-3 flex flex-col gap-2">
+        <h3 className="font-bold text-gray-900 text-xl">{TITLE_INSTALL_GUIDE}</h3>
+        <span className="ml-1 font-normal text-gray-500 text-xs md:text-base">
+          {DESC_INSTALL_GUIDE}
+        </span>
       </div>
-      <div className="grid min-w-[90vw] grid-cols-1 grid-rows-1 gap-4 md:grid-cols-4">
+      <div className="grid min-w-[90vw] grid-cols-1 grid-rows-1 gap-3 md:grid-cols-3">
         {INSTALL_GUIDE_STEPS.map((step) => {
           return (
-            <div key={step.index} className="flex flex-col">
-              <span>{step.title}</span>
-              <span>{step.description}</span>
+            <div key={step.index} className="flex flex-col gap-3 rounded bg-blue-50 p-3 md:p-5">
+              <div className="flex items-center gap-2">
+                <CheckIcon color={"#0F52BA"} />
+                <span className="font-semibold text-blue-800 text-sm md:text-base">
+                  {step.title}
+                </span>
+              </div>
+              <span className="font-normal text-gray-700 text-xs md:text-sm">
+                {step.description}
+              </span>
             </div>
           );
         })}
