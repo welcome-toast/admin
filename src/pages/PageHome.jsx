656efb130426a8ea05b97c25720f51f434b4d8f4
Jintae Song <w.jinttao@gmail.com>
2025-02-12 23:02:13 +0900

    Design: modify PageHome content, layout

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
 import Button from "../shared/Button";
-import { CTA_SAMPLE, CTA_SIGNIN, TITLE_HOME } from "../shared/constant";
+import { CTA_SAMPLE, CTA_SIGNIN, TITLE_DESC, TITLE_HOME } from "../shared/constant";
 
 import { useEffect } from "react";
 import { signIn, supabase } from "../shared/supabase";
@@ -36,13 +36,27 @@ function PageHome({ user, setUser }) {
   }, [user.id, setUser, navigate]);
 
   return (
-    <main className="mt-20 flex h-[85vh] w-full flex-col items-center justify-center gap-10">
-      <div>
-        <span className="font-bold text-5xl">{TITLE_HOME}</span>
+    <main className="mt-20 flex h-[80vh] w-full flex-col items-center justify-center gap-10 md:h-[85vh]">
+      <div className="flex flex-col gap-5 text-center">
+        <span className="font-bold text-2xl md:text-4xl">{TITLE_HOME}</span>
+        <span className="font-base text-md md:text-xl">{TITLE_DESC}</span>
       </div>
-      <div className="flex gap-5">
+      <div className="relative flex gap-5">
         <Button text={CTA_SIGNIN} onClick={handleSignInButtonClick} />
+        <div className="-right-1 -top-1 absolute">
+          <span className="relative flex size-3">
+            <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75" />
+            <span className="relative inline-flex size-3 rounded-full bg-blue-500" />
+          </span>
+        </div>
         <Button text={CTA_SAMPLE} onClick={handleSampleButtonClick} />
+      </div>
+      <div className="flex justify-center">
+        <img
+          src="/assets/home-introduction.png"
+          alt="home-introduction"
+          className="h-[30vh] w-full md:h-[50vh]"
+        />
       </div>
     </main>
   );
