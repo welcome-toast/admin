95161f0bb44a58771c274c2b722c2bd0d8f0b37f
Jintae Song <w.jinttao@gmail.com>
2025-02-13 17:00:11 +0900

    Design: add responsive design focus on editor

--- src/features/ProjectPreview.jsx
+++ src/features/ProjectPreview.jsx
@@ -38,27 +38,30 @@ const ProjectPreview = forwardRef(function ProjectPreview(
   }, [project?.api_key, project?.link, setIsMatchedProject, sendToastInput, firstToast]);
 
   return (
-    <div className="flex h-full w-full items-center justify-center px-1">
-      {!isLoadSuccedd && (
-        <div className="mt-10 flex flex-col items-center justify-center gap-5">
-          <Loading />
-          <span className="text-center">
-            연결된 웹사이트를 불러오고 있습니다.
-            <br />
-            로딩이 길어진다면 연동 여부를 다시 확인해주세요.
-          </span>
-        </div>
-      )}
-      {isMatchedProject && (
-        <iframe
-          id="projectPreview"
-          title="projectPreview"
-          ref={ref}
-          src={project?.link}
-          loading="lazy"
-          className={`${isLoadSuccedd ? "h-full w-full" : "h-1 w-1"} rounded-sm p-1`}
-        />
-      )}
+    <div className="flex h-full w-full flex-col justify-center gap-1">
+      <span className="mt-2 font-bold text-base text-gray-900 md:text-xl">미리 보기</span>
+      <div className="h-full w-full">
+        {!isLoadSuccedd && (
+          <div className="mt-10 flex flex-col items-center justify-center gap-5">
+            <Loading />
+            <span className="text-center text-xs lg:text-base">
+              연결된 웹사이트를 불러오고 있습니다.
+              <br />
+              로딩이 길어진다면 연동 여부를 다시 확인해주세요.
+            </span>
+          </div>
+        )}
+        {isMatchedProject && (
+          <iframe
+            id="projectPreview"
+            title="projectPreview"
+            ref={ref}
+            src={project?.link}
+            loading="lazy"
+            className={`${isLoadSuccedd ? "h-full w-full" : "h-1 w-1"} rounded-sm border p-1`}
+          />
+        )}
+      </div>
     </div>
   );
 });
--- src/widgets/ToastRedirectGuide.jsx
+++ src/widgets/ToastRedirectGuide.jsx
@@ -4,7 +4,7 @@ import CheckIcon from "../shared/Icon/CheckIcon";
 function ToastRedirectGuide({ isRedirect, title, description }) {
   return (
     <div
-      className={`fixed top-16 right-5 transition-all duration-500 ${isRedirect ? "opacity-100" : "opacity-0"} z-50 flex h-fit w-fit max-w-2xl items-start gap-2 rounded border border-orange-500 bg-orange-100 py-3 pr-8 pl-3 shadow-2xl`}
+      className={`fixed top-16 right-3 transition-all duration-500 md:right-5 ${isRedirect ? "opacity-100" : "opacity-0"} z-50 flex h-fit w-fit max-w-2xl items-start gap-2 rounded border border-orange-500 bg-orange-100 py-3 pr-8 pl-3 shadow-2xl`}
     >
       <div className="mt-1">
         <CheckIcon color={"#FF4D00"} />
