11f19d001795023fff3953cc09aab8ad6adc6181
Jintae Song <w.jinttao@gmail.com>
2025-03-29 14:54:44 +0900

    Refactor: change toast message warning to tsx

--- src/widgets/ToastWarning.tsx
+++ src/widgets/ToastWarning.tsx
@@ -1,7 +1,12 @@
-import PropTypes from "prop-types";
-import CheckIcon from "../shared/Icon/CheckIcon";
+import CheckIcon from "@/shared/Icon/CheckIcon";
+import type { ToastMessage } from "@/types";
 
-function ToastWarning({ warningType, title, description }) {
+interface ToastWarningProps {
+  warningType: string;
+  toastMessage: ToastMessage;
+}
+
+function ToastWarning({ warningType, toastMessage }: ToastWarningProps): JSX.Element {
   return (
     <div
       className={`fixed top-16 right-3 transition-all duration-500 md:right-5 ${warningType ? "opacity-100" : "opacity-0"} z-50 flex h-fit w-fit max-w-2xl items-start gap-2 rounded border border-orange-500 bg-orange-100 py-3 pr-8 pl-3 shadow-2xl`}
@@ -10,17 +15,11 @@ function ToastWarning({ warningType, title, description }) {
         <CheckIcon color={"#FF4D00"} />
       </div>
       <div className="flex flex-col gap-1">
-        <span className="font-semibold text-base">{title}</span>
-        <span className="font-normal text-sm">{description}</span>
+        <span className="font-semibold text-base">{toastMessage.title}</span>
+        <span className="font-normal text-sm">{toastMessage.description}</span>
       </div>
     </div>
   );
 }
 
 export default ToastWarning;
-
-ToastWarning.propTypes = {
-  warningType: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-};
