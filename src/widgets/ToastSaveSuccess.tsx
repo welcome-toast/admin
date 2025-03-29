3bdb5fb41b11c891092e75e820b73757a81f14c6
Jintae Song <w.jinttao@gmail.com>
2025-03-29 14:52:41 +0900

    Refactor: change toast message success to tsx

--- src/widgets/ToastSaveSuccess.tsx
+++ src/widgets/ToastSaveSuccess.tsx
@@ -1,7 +1,12 @@
-import PropTypes from "prop-types";
-import CheckIcon from "../shared/Icon/CheckIcon";
+import CheckIcon from "@/shared/Icon/CheckIcon";
+import type { ToastMessage } from "@/types";
 
-function ToastSaveSuccess({ isToastSaved, title, description }) {
+interface ToastSaveSuccessProps {
+  isToastSaved: boolean;
+  toastMessage: ToastMessage;
+}
+
+function ToastSaveSuccess({ isToastSaved, toastMessage }: ToastSaveSuccessProps): JSX.Element {
   return (
     <div
       className={`fixed top-16 right-3 transition-all duration-500 md:right-5 ${isToastSaved ? "opacity-100" : "opacity-0"} z-50 flex h-fit w-fit max-w-2xl items-start gap-2 rounded border border-blue-700 bg-blue-100 py-3 pr-8 pl-3 shadow-2xl`}
@@ -10,17 +15,11 @@ function ToastSaveSuccess({ isToastSaved, title, description }) {
         <CheckIcon color={"blue"} />
       </div>
       <div className="flex flex-col gap-1">
-        <span className="font-semibold text-base">{title}</span>
-        <span className="font-normal text-sm">{description}</span>
+        <span className="font-semibold text-base">{toastMessage.title}</span>
+        <span className="font-normal text-sm">{toastMessage.description}</span>
       </div>
     </div>
   );
 }
 
 export default ToastSaveSuccess;
-
-ToastSaveSuccess.propTypes = {
-  isToastSaved: PropTypes.bool.isRequired,
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-};
